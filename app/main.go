9d1f5d25c84e18beea1bccc8e15a51e1ae944642
Joel-ufg <joelrezende@discente.ufg.br>
2025-06-03 20:25:12 -0300

    Update main.go

--- app/main.go
+++ app/main.go
@@ -4,21 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
-func main() {
+func openBrowser(url string) {
+	var err error
 
+	switch runtime.GOOS {
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin": // macOS
+		err = exec.Command("open", url).Start()
+	default: // Linux
+		err = exec.Command("xdg-open", url).Start()
+	}
+
+	if err != nil {
+		log.Printf("Falha ao abrir o navegador: %v\n", err)
+	}
+}
+
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
-			http.Redirect(w, r, "/Login/Login.html", http.StatusFound)
+			http.Redirect(w, r, "/login/login.html", http.StatusFound)
 			return
 		}
 		fileServer.ServeHTTP(w, r)
 	})
 
-	fmt.Println("Servidor rodando em http://localhost:8081/")
+	url := "http://localhost:8081/"
+	fmt.Println("Servidor rodando em", url)
+
+	openBrowser(url)
+
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
